ba454c47ed7b51aec47e94fd7ad68bccf4eb08eb
huangqiang <huangqiang@onespacechina.com>
2019-07-15 10:16:45 +0800

    update main control node logic about sending command

--- driver/mainctrl.c
+++ driver/mainctrl.c
@@ -4,12 +4,7 @@
 #include "timer.h"
 #include "mainctrl.h"
 #include <stdbool.h>
-
-#define SLAVE0_WKUP (1 << 0)
-#define SLAVE1_WKUP (1 << 1)
-#define SLAVE2_WKUP (1 << 2)
-#define SLAVE3_WKUP (1 << 3)
-#define SLAVE_WKUP_MSK (0x0f)
+#include <string.h>
 
 volatile uint8_t g_slaveStatus = 0;
 
@@ -17,39 +12,86 @@ volatile uint8_t g_slaveStatus = 0;
 
 extern volatile uint32_t g_Ticks;
 
-void initFrame(struct MainCtrlFrame *mainCtrlFr, uint8_t src, uint8_t slave, uint8_t type)
+uint16_t CalFrameCRC(struct MainCtrlFrame *mainCtrlFr)
 {
-	memset(mainCtrlFr, 0x00, sizeof(*mainCtrlFr));
+	int i = 0;
+	uint16_t crc_sum = 0;
+
+	for (; i < FRAME_DATA_LEN; i++)
+		crc_sum += mainCtrlFr->data[i];
+
+	return crc_sum;
+}
+
+void InitFrame(struct MainCtrlFrame *mainCtrlFr, uint8_t src, uint8_t slave,
+		uint8_t type, uint8_t data[])
+{
+	uint16_t data_crc = 0;
+
+	memset(mainCtrlFr, 0xff, sizeof(*mainCtrlFr));
 	mainCtrlFr->head0 = 0x55;
 	mainCtrlFr->head1 = 0xaa;
 	mainCtrlFr->len = 0;
 	mainCtrlFr->frameCtrl = ((src & 0x03) < 6) | (slave & 0x7);
 	mainCtrlFr->frameType = type & 0xf;
+	memcpy(mainCtrlFr->data, data, FRAME_DATA_LEN);
+
+	data_crc = CalFrameCRC(mainCtrlFr);
+	mainCtrlFr->crc0 = data_crc & 0xff;
+	mainCtrlFr->crc1 = (data_crc >> 8) & 0xff;
 }
 
-uint8_t CheckSlave(uint8_t src, uint8_t slave, uint8_t type)
+/*
+ * send a frame to slave and poll the corresponding back token.
+ * @src: frame source, main node, manual node or slave node
+ * @slave: talk to which slave node
+ * @type£º frame type
+ *
+ * @ret: -1: talk timeout; 0: talk successfully.
+ * */
+uint8_t TalktoSlave(uint8_t src, uint8_t slave, uint8_t type, uint8_t data[])
 {
-	initFrame(&g_mainCtrlFr, src, slave, type);
+	int8_t ret = -1;
+
+	InitFrame(&g_mainCtrlFr, src, slave, type, data);
 
 	/*
-	 * to do:  wireless send loggic
+	 * reset command timeout
+	 * */
+	g_cmd_feedback_timeout = g_Ticks + CMD_FEEDBACK_TIMEOUT;
+
+	/*
+	 * to do:  wireless send logic
 	 * */
 
 	while (g_Ticks < g_cmd_feedback_timeout) {
 
 	}
+
+	return ret;
 }
 
 void WakeupSlave(void)
 {
+	int i = 0, ret = -1;
+	uint8_t fr_data[FRAME_DATA_LEN];
+
+	memset(fr_data, 0xff, FRAME_DATA_LEN);
 	g_wakup_timeout = g_Ticks + WAKUP_DURATION;
 
-	g_cmd_feedback_timeout = g_Ticks + CMD_FEEDBACK_TIMEOUT;
-
 	while (g_Ticks < g_wakup_timeout) {
+		for (i = 0; i < SLAVE_NUMS; i++) {
+			ret = 	TalktoSlave(MAIN_NODE, i, ENUM_SAMPLE_SET, fr_data);
+			if (ret == 0)
+				g_slaveStatus |= (1 << i);
+		}
 
+		/*
+		 * all slaves are waken up
+		 * */
+		if ((g_slaveStatus & SLAVE_WKUP_MSK) == SLAVE_WKUP_MSK)
+			break;
 	}
-
 }
 
 void RecvFromSlave(void)
--- include/mainctrl.h
+++ include/mainctrl.h
@@ -3,6 +3,44 @@
 #ifndef INLCUDE_MAINCTRL_H_
 #define INLCUDE_MAINCTRL_H_
 
+/*
+ * slave waken up flag
+ * */
+#define SLAVE0_WKUP (1 << 0)
+#define SLAVE1_WKUP (1 << 1)
+#define SLAVE2_WKUP (1 << 2)
+#define SLAVE3_WKUP (1 << 3)
+#define SLAVE_WKUP_MSK (0x0f)
+
+/*
+ * CMD frame information
+ * */
+#define MAIN_NODE (0 << 6)
+#define MANUAL_NODE (1 << 6)
+#define SLAVE_NODE (2 << 6)
+
+#define SLAVE_NODE_0 0x00
+#define SLAVE_NODE_1 0x01
+#define SLAVE_NODE_2 0x02
+#define SLAVE_NODE_3 0x03
+#define SLAVE_BROADCAST 0x07
+
+/*
+ * frame type
+ * */
+enum {
+	ENUM_SAMPLE_SET = 0x01,
+	ENUM_SAMPLE_SET_TOKEN,
+	ENUM_SAMPLE_DATA,
+	ENUM_SAMPLE_DATA_TOKEN,
+	ENUM_SLAVE_STATUS,
+	ENUM_SLAVE_STATUS_TOKEN,
+	ENUM_SLAVE_SLEEP,
+	ENUM_SLAVE_SLEEP_TOKEN
+};
+
+#define FRAME_DATA_LEN 5
+
 struct MainCtrlFrame {
 	uint8_t head0; //0x55
 	uint8_t head1; //0xaa
@@ -10,7 +48,7 @@ struct MainCtrlFrame {
 	uint8_t serial; // serial num: 0-255
 	uint8_t frameCtrl;
 	uint8_t frameType;
-	uint8_t data[5];
+	uint8_t data[FRAME_DATA_LEN];
 	uint8_t crc0; // crc[7:0]
 	uint8_t crc1; // crc[15:8]
 };
@@ -30,5 +68,7 @@ struct BackTokenFrame {
 };
 
 struct BackTokenFrame g_backTokenFr;
+
+extern uint16_t CalFrameCRC(struct MainCtrlFrame *mainCtrlFr);
 
 #endif
